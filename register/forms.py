d35e32cb7cbd51328f81ecce4b591298ec7661b7
tamanarasuli <tamana.rasuli@gmail.com>
2020-10-03 22:39:21 -0500

    fixed register

--- register/forms.py
+++ register/forms.py
@@ -12,3 +12,18 @@ class student(Forms.ModelForm):
     contactInfo = models.IntegerField()
     description = models.CharField(max_length = 100)
     user = models.OneToOneField(User,on_delete= models.CASCADE)
+
+class SignUpForm(UserCreationForm): 
+    first_name= forms.CharField(max_length = 10)
+    last_name = forms.CharField(max_length = 10)
+    user_name = forms.CharField(max_length = 20)
+
+    class Meta: 
+        model = User
+        fields = [
+            'first_name', 
+            'last_name', 
+            'user_name', 
+            'password_1',
+            'password_2', 
+        ]
--- register/views.py
+++ register/views.py
@@ -3,7 +3,13 @@ from .models import student, mentor
 
 def register(request):
     if request.method == 'POST':
-        return 
+        
+        if request.POST.get('student'):
+            return render(request, 'register/student_register.html')
+        else:
+            return render(request, 'register/mentor_register.html')
+        #return
+    return render(request, 'register/register.html')
     if request.user.is_authenticated:
         newstudent = student.objects.create(
             user = request.user, 
@@ -17,4 +23,14 @@ def register(request):
             description = 'test example', 
         )
         newstudent.save()
-        
+
+def mentor_register():
+    if request.POST.get('first_name', 'last_name','education', 'zipcode' 'Credentials', 'contact_info', 'area_of_interest'):
+        return 
+
+def student_register():
+    if request.POST.get('first_name', 'last_name', 'major', 'zipcode', 'school_year', 'contact_info', 'area_of_interest'):
+        return
+
+
+
