fac430c6a8b85f1313d55c43466bb9d250724997
Kenneth <ngken0995@gmail.com>
2020-10-04 01:02:08 -0400

    update

--- register/views.py
+++ register/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.http import HttpResponseRedirect
+from django.contrib.auth import login
 from .models import Student, Mentor, User, Match 
 
 def register(request):
@@ -12,6 +13,7 @@ def register(request):
             request.POST.get('password'),
         )
         newuser.save()
+        login(request,newuser)
         if request.POST.get('student'):
             return render(request, 'register/student_register.html')
         else:
@@ -23,13 +25,13 @@ def mentor_register(request):
     if request.method == 'POST': 
         newmentor = Mentor.objects.create (
             user = request.user, 
-            f_name = 'first_name', 
-            l_name = 'last_name', 
-            education = 'education', 
-            zipcode = 'zipcode', 
-            Credentials = 'Credentials', 
-            contact_info = 'contact_info', 
-            area_of_interest = 'area_of_interest'
+            Fname = request.POST.get('first_name'), 
+            Lname = request.POST.get('last_name'), 
+            education = request.POST.get('education'), 
+            zipcode = request.POST.get('zipcode'), 
+            Credentials = request.POST.get('Credentials'), 
+            contactInfo = request.POST.get('contact_info'),
+            online = int(request.POST.get('online'))
 
         )
         newmentor.save()
@@ -40,13 +42,13 @@ def student_register(request):
     if request.method == 'POST': 
         newstudent = Student.objects.create (
             user = request.user, 
-            f_name = 'first_name', 
-            l_name = 'last_name', 
-            major = 'major', 
-            zipcode = 'zipcode', 
-            school_year = 'school_year', 
-            contact_info = 'contact_info', 
-            area_of_interest = 'area_of_interest'
+            Fname = request.POST.get('first_name'), 
+            Lname = request.POST.get('last_name'), 
+            major = request.POST.get('major'), 
+            zipcode = request.POST.get('zipcode'), 
+            schoolYear = request.POST.get('school_year'), 
+            contactInfo = request.POST.get('contact_info'),
+            online = int(request.POST.get('online'))
 
         )
         newstudent.save()
